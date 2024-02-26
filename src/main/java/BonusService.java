8c6afaa51a573b6bb6724868ab23fa4cac7c9a04
Natalia Kuzmicheva <kuzmichyova.natalie@yandex.ru>
2024-02-26 15:24:58 +0300

    Rename percent to Percent

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -1,7 +1,7 @@
 public class BonusService {
     public long calculate(long amount, boolean registered) {
-        int percent = registered ? 3 : 1; // процент считается для зарегистрированного пользователя 3 процента, незарегистрированного 1 процент
-        long bonus = amount * percent / 100;
+        int Percent = registered ? 3 : 1; // процент считается для зарегистрированного пользователя 3 процента, незарегистрированного 1 процент
+        long bonus = amount * Percent / 100;
         long limit = 500;
         if (bonus > limit) {
             bonus = limit;
